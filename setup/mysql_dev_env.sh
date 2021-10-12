e79fc1c6ef29e7762ee508011b93df122798f367
Gaby Dias <gabydias@gmail.com>
2021-10-12 03:53:50 -0300

    travis 19.0

--- setup/mysql_dev_env.sh
+++ setup/mysql_dev_env.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker run --name=mysqldb -e MYSQL_ROOT_HOST=% -e MYSQL_ROOT_PASSWORD=admin -p 3306:3306 -d mysql/mysql-server:5.7
 
 mysql -h 127.0.0.1 -P 3306 -u root -p -e "CREATE USER 'playuser'@'%' IDENTIFIED BY '123456';"
@@ -7,5 +9,3 @@ mysql -h 127.0.0.1 -P 3306 -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO 'playus
 mysql -h 127.0.0.1 -P 3306 -u playuser -p < Playlist.sql
 
 mysql -h 127.0.0.1 -P 3306 -u playuser -p playlist -e "show tables;"
-
-
