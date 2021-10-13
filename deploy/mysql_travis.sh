337796984ea72e022e798d1d94679d978db814f3
Gaby Dias <gabydias@gmail.com>
2021-10-12 21:44:01 -0300

    travis 23.0

--- deploy/mysql_travis.sh
+++ deploy/mysql_travis.sh
@@ -4,4 +4,5 @@ mysql -e "CREATE USER '$CLOUD_SQL_USERNAME'@'%' IDENTIFIED BY '$CLOUD_SQL_PASSWO
 mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$CLOUD_SQL_USERNAME'@'%' WITH GRANT OPTION;"
 echo -e "[client]\nuser=$CLOUD_SQL_USERNAME\npassword=$CLOUD_SQL_PASSWORD" > ~/.my.cnf
 chmod 600 ~/.my.cnf
+
 mysql -u $CLOUD_SQL_USERNAME < setup/Playlist.sql
--- test.py
+++ test.py
@@ -2,10 +2,8 @@
 from app import app
 import os
 import unittest
-import mysql.connector
 
 class Test(unittest.TestCase):
-    connection = None
 
     def setUp(self):
         # cria uma instância do unittest, precisa do nome "setUp"
@@ -13,19 +11,6 @@ class Test(unittest.TestCase):
 
         # envia uma requisicao GET para a URL
         self.result = self.app.get('/')
-
-        db_user = os.environ.get('CLOUD_SQL_USERNAME')
-        db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-        db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-        db_local_host = os.environ.get('DB_LOCAL_HOST')
-        self.connection = mysql.connector.connect(user=db_user, password=db_password, host=db_local_host, database=db_name)
-
-    def tearDown(self):
-        if self.connection is not None and self.connection.is_connected():
-            self.connection.close()
-
-    def test_connection(self):
-        self.assertTrue(self.connection.is_connected())
 
     def test_requisicao(self):
         # compara o status da requisicao (precisa ser igual a 200)
