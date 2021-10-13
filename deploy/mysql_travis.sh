147773c02851546daac08d081f286f7a3218b529
Gaby Dias <gabydias@gmail.com>
2021-10-12 22:02:35 -0300

    travis 26.0

--- deploy/mysql_travis.sh
+++ deploy/mysql_travis.sh
@@ -5,4 +5,4 @@ mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$CLOUD_SQL_USERNAME'@'%' WITH GRANT OP
 echo -e "[client]\nuser=$CLOUD_SQL_USERNAME\npassword=$CLOUD_SQL_PASSWORD" > ~/.my.cnf
 chmod 600 ~/.my.cnf
 
-mysql -u $CLOUD_SQL_USERNAME < setup/Playlist.sql
+mysql -u $CLOUD_SQL_USERNAME < deploy/Playlist.sql
